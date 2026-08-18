94dafe035d57cf64be7e3de00ef79a65722c8474
sawa <sawai6101@yahoo.co.jp>
2026-08-18 15:35:19 +0900

    Add sample circle data

---
+++ src/data/circles.ts
@@ -0,0 +1,8 @@
+import { CircleData } from "../types";
+export const initialCircles: CircleData[] = [
+{id:"A-01",space:"東1 A-01",name:"サンプルサークル01",status:"UNVISITED",x:180,y:270},
+{id:"A-02",space:"東1 A-02",name:"サンプルサークル02",status:"UNVISITED",x:250,y:320},
+{id:"A-03",space:"東1 A-03",name:"サンプルサークル03",status:"COMPLETED",x:330,y:370},
+{id:"B-01",space:"東2 B-01",name:"サンプルサークル04",status:"SOLD_OUT",x:630,y:270},
+{id:"B-02",space:"東2 B-02",name:"サンプルサークル05",status:"UNVISITED",x:720,y:330}
+];
